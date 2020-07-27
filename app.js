d86aae660c2de7668ee3107c2218639ea18f182a
dihu-ciq <di@chargeiq.de>
2020-07-27 09:57:53 +0200

    remove server.js

--- app.js
+++ app.js
@@ -24,6 +24,25 @@ app.post('/link', function(req,res) {
     res.send("Buy something at us! " + affilateObject.year + " "+affilateObject.purchaseObject+ " " + affilateObject.company);
 });
 
+//JSON object to be added to cookie 
+let users = { 
+    name : "ChargeIQ", 
+    Age : "30"
+} 
+    
+//Route for adding cookie 
+app.get('/setuser', (req, res)=>{ 
+    res.cookie("userData", users); 
+    res.send('user data added to cookie'); 
+}); 
+    
+//Iterate users data from cookie 
+app.get('/getuser', (req, res)=>{ 
+    //shows all the cookies 
+    res.send(req.cookies); 
+}); 
+      
+
 app.listen(port, function()
     {
         console.log(`Example app listening at http://localhost:${port}`)
