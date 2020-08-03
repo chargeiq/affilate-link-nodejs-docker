4ea543c3a311eff3282698d1f311c28ba60e4c74
dihu-ciq <di@chargeiq.de>
2020-08-03 13:34:13 +0200

    removed cookie routine

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ var dbController = require('./couchdb-controller.js')
 
 //HTTP Port
 var port = 8181;
-
 var app = express();
 app.use(cookieParser());
 
@@ -24,26 +23,26 @@ app.post('/link', function(req,res) {
     res.send("Buy something at us! " + affilateObject.date + " "+affilateObject.purchaseObject+ " " + affilateObject.company);
 });
 
+/*
 //JSON object to be added to cookie 
 let affilateUser = { 
     name : "ChargeIQ", 
     Age : "30",
     company : "enbw"
-} 
+};
     
 //Route for adding cookie 
 app.get('/setuser', (req, res)=>{ 
-    res.cookie("userData", affilateUser); 
-    res.send('user data added to cookie'); 
+    res.cookie("userData", affilateUser,  {maxAge: 300000}); //The cookie will be killed after 300 seconds.
+    res.send('user data ' + affilateUser.name + ' added to cookie'); 
 }); 
     
 //Iterate users data from cookie 
 app.get('/getuser', (req, res)=>{ 
     //shows all the cookies 
     res.send(req.cookies); 
-}); 
+}); */
       
-
 app.listen(port, function()
 {
     console.log(`Example app listening at http://localhost:${port}`)
